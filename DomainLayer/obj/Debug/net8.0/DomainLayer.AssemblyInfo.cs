e210e58794392ba537d3198b210b22500f9b7fc3
Utsav Basyal <utsav.basyal@dishhome.com.np>
2024-11-26 09:59:50 +0545

    added entities, services, interfaces and repository folder

--- DomainLayer/obj/Debug/net8.0/DomainLayer.AssemblyInfo.cs
+++ DomainLayer/obj/Debug/net8.0/DomainLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DomainLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a3c4cde1883037d830755126288ae8e802da8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06f99b75021b19b4e12ab4b4b5bc6bba14dd178a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DomainLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DomainLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
